507c8fba924feab19ed5eac828b14ad89235bfc6
云舒 <im@yunshu.site>
2023-09-06 11:05:01 +0000

    chore: prepare for migration to GitHub * chore: prepare for migration to GitHub

--- .github/pull_request_template.md
+++
@@ -1,9 +0,0 @@
-### What problem were solved in this pull request?
-
-Issue Number: close #xxx
-
-Problem:
-
-### What is changed and how it works?
-
-### Other information
--- format.py
+++ format.py
@@ -10,6 +10,9 @@ files.extend(heard_files)
 # 如果路径中包含 deps/3rd 则不进行格式化
 files = [i for i in files if "deps/3rd" not in i]
 
+# 如果路径中包含 sql/parser 则不进行格式化
+files = [i for i in files if "sql/parser" not in i]
+
 for i in files:
     os.system(f"clang-format --style=file -i \"%s\"" % i)
 
