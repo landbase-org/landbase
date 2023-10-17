9a1e64f72703e95b63130f66fbf0cac0d4365625
xym <1328699220@qq.com>
2023-10-17 13:17:38 +0800

    feat: join-tables but got terriable performance

--- src/observer/sql/parser/parse_defs.h
+++ src/observer/sql/parser/parse_defs.h
@@ -268,6 +268,16 @@ struct ConditionSqlNode
 };
 
 /**
+ * @brief 描述一个Join语句
+ * @ingroup SQLParser
+ */
+struct JoinSqlNode
+{
+  std::string join_relation;
+  std::vector<ConditionSqlNode> join_conditions;
+};
+
+/**
  * @brief 描述一个select语句
  * @ingroup SQLParser
  * @details 一个正常的select语句描述起来比这个要复杂很多，这里做了简化。
@@ -283,6 +293,7 @@ struct SelectSqlNode
   std::vector<RelAttrSqlNode>   attributes;  ///< attributes in select clause
   std::vector<std::string>      relations;   ///< 查询的表
   std::vector<ConditionSqlNode> conditions;  ///< 查询条件，使用AND串联起来多个条件
+  std::vector<JoinSqlNode>      joinctions;  ///< Join-list
 };
 
 /**
